eaa7790f6104248099a8870662d839257a5effd2
[dyad] <tdisawas0+github@gmail.com>
2025-05-25 19:11:11 +0700

    Reverted all changes back to version 3198da991a73feae0b94f3158b1959e441d52672

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
 
-createRoot(document.getElementById("root")!).render(
-  <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-    <App />
-  </ThemeProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,7 @@
 import TodoListPage from "./TodoListPage";
-import { ThemeToggle } from "@/components/ThemeToggle"; // Import ThemeToggle
 
 const Index = () => {
-  return (
-    <div className="relative"> {/* Add a container for positioning */}
-      <div className="absolute top-4 right-4 z-10"> {/* Position the toggle button */}
-        <ThemeToggle />
-      </div>
-      <TodoListPage />
-    </div>
-  );
+  return <TodoListPage />;
 };
 
 export default Index;
