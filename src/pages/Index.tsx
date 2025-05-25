0be497079405c093ff3e3601ebe8c2362364fe4f
[dyad] <tdisawas0+github@gmail.com>
2025-05-25 19:10:36 +0700

    [dyad] Added dark mode toggle functionality. - wrote 3 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,15 @@
 import TodoListPage from "./TodoListPage";
+import { ThemeToggle } from "@/components/ThemeToggle"; // Import ThemeToggle
 
 const Index = () => {
-  return <TodoListPage />;
+  return (
+    <div className="relative"> {/* Add a container for positioning */}
+      <div className="absolute top-4 right-4 z-10"> {/* Position the toggle button */}
+        <ThemeToggle />
+      </div>
+      <TodoListPage />
+    </div>
+  );
 };
 
 export default Index;
